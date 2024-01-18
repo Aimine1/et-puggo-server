e9062553b7b782af4e3a8790c6bb23dcb9eb2d63
= <niuzhenyuself@163.com>
2024-01-18 14:13:16 +0800

    商品详情增加 isVerified字段 是否认

--- src/main/java/com/etrade/puggo/dao/user/UserDao.java
+++ src/main/java/com/etrade/puggo/dao/user/UserDao.java
@@ -1,8 +1,5 @@
 package com.etrade.puggo.dao.user;
 
-import static com.etrade.puggo.db.Tables.USER;
-import static com.etrade.puggo.db.Tables.USER_IM_ACTION;
-
 import com.etrade.puggo.common.page.PageContentContainer;
 import com.etrade.puggo.dao.BaseDao;
 import com.etrade.puggo.service.account.SellerInfoVO;
@@ -11,14 +8,18 @@ import com.etrade.puggo.service.account.WebUserSearchParam;
 import com.etrade.puggo.service.goods.sales.pojo.LaunchUserDO;
 import com.etrade.puggo.utils.SQLUtils;
 import com.etrade.puggo.utils.StrUtils;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
 import org.jooq.SelectJoinStep;
 import org.jooq.impl.DSL;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static com.etrade.puggo.db.Tables.USER;
+import static com.etrade.puggo.db.Tables.USER_IM_ACTION;
 
 /**
  * @author niuzhenyu
@@ -36,10 +37,10 @@ public class UserDao extends BaseDao {
 
     public Long insertNewUser(String nickname, String email, String role) {
         return db
-            .insertInto(USER, USER.NICKNAME, USER.NICKNAME_LAST_TIME, USER.EMAIL, USER.USER_ROLE, USER.JOIN_DATE,
-                USER.AVATAR)
-            .values(nickname, LocalDateTime.now(), email, role, LocalDate.now(), this.avatar)
-            .returning(USER.ID).fetchOne().getId();
+                .insertInto(USER, USER.NICKNAME, USER.NICKNAME_LAST_TIME, USER.EMAIL, USER.USER_ROLE, USER.JOIN_DATE,
+                        USER.AVATAR)
+                .values(nickname, LocalDateTime.now(), email, role, LocalDate.now(), this.avatar)
+                .returning(USER.ID).fetchOne().getId();
     }
 
 
@@ -53,67 +54,68 @@ public class UserDao extends BaseDao {
 
     public UserInfoVO findUserInfo(Long userId) {
         return db.select(
-                USER.USER_ROLE,
-                USER.NICKNAME,
-                USER.AVATAR,
-                USER.EMAIL,
-                USER.JOIN_DATE,
-                USER.CREDIT_RATING,
-                USER.PHONE,
-                DSL.iif(USER.IS_VERIFIED.eq(IS_VERIFIED), true, false).as("isVerified"),
-                USER.COUNTRY.concat(" ").concat(USER.PROVINCE).concat(" ").concat(USER.CITY).as("region")
-            )
-            .from(USER).where(USER.ID.eq(userId)).fetchAnyInto(UserInfoVO.class);
+                        USER.USER_ROLE,
+                        USER.NICKNAME,
+                        USER.AVATAR,
+                        USER.EMAIL,
+                        USER.JOIN_DATE,
+                        USER.CREDIT_RATING,
+                        USER.PHONE,
+                        DSL.iif(USER.IS_VERIFIED.eq(IS_VERIFIED), true, false).as("isVerified"),
+                        USER.COUNTRY.concat(" ").concat(USER.PROVINCE).concat(" ").concat(USER.CITY).as("region")
+                )
+                .from(USER).where(USER.ID.eq(userId)).fetchAnyInto(UserInfoVO.class);
     }
 
 
     public SellerInfoVO findSellerInfo(Long userId) {
         return db.select(
-                USER.USER_ROLE,
-                USER.NICKNAME,
-                USER.AVATAR,
-                USER.EMAIL,
-                USER.JOIN_DATE,
-                USER.CREDIT_RATING,
-                DSL.iif(USER.IS_VERIFIED.eq(IS_VERIFIED), true, false).as("isVerified"),
-                USER.COUNTRY.concat(" ").concat(USER.PROVINCE).concat(" ").concat(USER.CITY).as("region")
-            )
-            .from(USER).where(USER.ID.eq(userId)).fetchAnyInto(SellerInfoVO.class);
+                        USER.USER_ROLE,
+                        USER.NICKNAME,
+                        USER.AVATAR,
+                        USER.EMAIL,
+                        USER.JOIN_DATE,
+                        USER.CREDIT_RATING,
+                        DSL.iif(USER.IS_VERIFIED.eq(IS_VERIFIED), true, false).as("isVerified"),
+                        USER.COUNTRY.concat(" ").concat(USER.PROVINCE).concat(" ").concat(USER.CITY).as("region")
+                )
+                .from(USER).where(USER.ID.eq(userId)).fetchAnyInto(SellerInfoVO.class);
     }
 
 
     public int updateUserAvatar(Long userId, String url) {
         return db.update(USER)
-            .set(USER.AVATAR, url)
-            .where(USER.ID.eq(userId))
-            .execute();
+                .set(USER.AVATAR, url)
+                .where(USER.ID.eq(userId))
+                .execute();
     }
 
 
     public List<LaunchUserDO> findUserList(List<Long> userIdList) {
-        return db.select(USER.ID.as("userId"), USER.AVATAR, USER.NICKNAME, USER_IM_ACTION.ACCID)
-            .from(USER)
-            .leftJoin(USER_IM_ACTION)
-            .on(USER_IM_ACTION.USER_ID.eq(USER.ID))
-            .where(USER.ID.in(ascendingOrder(userIdList)))
-            .fetchInto(LaunchUserDO.class);
+        return db.select(USER.ID.as("userId"), USER.AVATAR, USER.NICKNAME, USER_IM_ACTION.ACCID,
+                        DSL.iif(USER.IS_VERIFIED.eq(IS_VERIFIED), true, false).as("isVerified"))
+                .from(USER)
+                .leftJoin(USER_IM_ACTION)
+                .on(USER_IM_ACTION.USER_ID.eq(USER.ID))
+                .where(USER.ID.in(ascendingOrder(userIdList)))
+                .fetchInto(LaunchUserDO.class);
     }
 
 
     public PageContentContainer<UserInfoVO> findUserPageList(WebUserSearchParam param) {
         SelectJoinStep<?> sql = db.select(
-                USER.ID.as("userId"),
-                USER.USER_ROLE,
-                USER.NICKNAME,
-                USER.AVATAR,
-                USER.EMAIL,
-                USER.JOIN_DATE,
-                USER.CREDIT_RATING,
-                USER.PHONE,
-                DSL.iif(USER.IS_VERIFIED.eq(IS_VERIFIED), true, false).as("isVerified"),
-                USER.COUNTRY.concat(" ").concat(USER.PROVINCE).concat(" ").concat(USER.CITY).as("region")
-            )
-            .from(USER);
+                        USER.ID.as("userId"),
+                        USER.USER_ROLE,
+                        USER.NICKNAME,
+                        USER.AVATAR,
+                        USER.EMAIL,
+                        USER.JOIN_DATE,
+                        USER.CREDIT_RATING,
+                        USER.PHONE,
+                        DSL.iif(USER.IS_VERIFIED.eq(IS_VERIFIED), true, false).as("isVerified"),
+                        USER.COUNTRY.concat(" ").concat(USER.PROVINCE).concat(" ").concat(USER.CITY).as("region")
+                )
+                .from(USER);
 
         if (!StrUtils.isBlank(param.getNickname())) {
             sql.where(USER.NICKNAME.like(SQLUtils.suffixLike(param.getNickname())));
